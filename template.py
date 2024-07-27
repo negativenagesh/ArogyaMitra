f28dbe69b611a2650821f8fd8f97dae9f11db08b
Subrahmanya Gaonkar <gaonkarsub@gmail.com>
2024-07-27 15:22:19 +0530

    added for loop to create folder/files

--- template.py
+++ template.py
@@ -16,3 +16,19 @@ list_of_files=[
     "static",
     "templates/chat.html"
 ]
+
+for filepath in list_of_files:
+    filepath=Path(filepath)
+    filedir,filename=os.path.split(filepath)
+    
+    if filedir!= "":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"creating directory {filedir} for the file {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filename)==0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{filename} is already created")
