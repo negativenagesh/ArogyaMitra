b2c65d77045b952c3b818b2eec2bb99edd4c20fb
Subrahmanya Gaonkar <gaonkarsub@gmail.com>
2024-07-27 15:11:16 +0530

    added list of files needed

--- template.py
+++ template.py
@@ -4,4 +4,15 @@ import logging
 
 logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:%(message)s')
 
-
+list_of_files=[
+    "src/__init__.py",
+    "src/helper.py",
+    "src/prompt.py",
+    ".env",
+    "setup.py",
+    "research/trials.ipynb",
+    "app.py",
+    "store_index.py",
+    "static",
+    "templates/chat.html"
+]
