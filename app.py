da7d2955f7c95dbdcbf419c1711d54b47a712779
Subrahmanya Gaonkar <gaonkarsub@gmail.com>
2024-07-28 10:08:52 +0530

    added required libraries

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, jsonify, request
+from src.helper import download_hugging_face_embeddings
+from langchain.vectorstores import Pinecone
+import pinecone
+from langchain.prompts import PromptTemplate
+from langchain.llms import CTransformers
+from langchain.chains import RetrievalQA
+from dotenv import load_dotenv
