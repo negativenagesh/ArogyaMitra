44c1355c9f357c999997eb64eeedee2ba4e22b50
Subrahmanya Gaonkar <gaonkarsub@gmail.com>
2024-07-29 22:55:55 +0530

    added host and port

--- app.py
+++ app.py
@@ -64,5 +64,9 @@ qa = RetrievalQA.from_chain_type(
 def index():
     return render_template('chat.html')
 
+
+
+
+
 if __name__ =='__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=8080,debug=True)
